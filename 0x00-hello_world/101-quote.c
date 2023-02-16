4a3ff46d21779c4e0dca0163ed43ca99ab95110c
dikachi188 <oowonta@gmail.com>
2023-02-16 15:31:11 -0800

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ *  main - A progarm that print a line using the unix function
+ * Return: 1 (success)
+ */
+int main(void)
+{
+fput ("and that piece of art is useful\"  Dora Korpar, 2015-10-19\n", stdout);
+return(1);
+}
