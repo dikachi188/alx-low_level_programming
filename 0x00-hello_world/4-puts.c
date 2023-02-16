71eb86eca6ba2e7f0db6692ebac095ee74c61e98
dikachi188 <oowonta@gmail.com>
2023-02-16 13:55:20 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include < stdio.h >
+/**
+ * main -  A C program that prints exactly Programming is like by a new line
+ * Return: 0 (successful)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
